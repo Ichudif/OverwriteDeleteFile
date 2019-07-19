181c5589efd7a349ed856c37579ea374b7fcafcd
Andreas Harapatt <andreas.harapatt@studierende.htl-donaustadt.at>
2019-07-20 00:55:57 +0200

    Add project files.

---
+++ OverwriteDeleteFiles/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsInput;
+using WindowsInput.Native;
+
+namespace OverwriteDeleteFiles
+{
+    class Program
+    {
+        private static UtilityLibrary.UtilityLibrary ul;
+        private static Random rnd = new Random();
+
+        [STAThread]
+        static void Main(string[] args)
+        {
+            ul = new UtilityLibrary.UtilityLibrary();
+            ul.startlooking(Keys.Delete);
+            ul.KeyPressedEvent += Ul_KeyPressedEvent;
+
+            while (true)
+                Thread.Sleep(1000);
+        }
+
+        private static void Ul_KeyPressedEvent()
+        {
+            Thread t = new Thread(() =>
+            {
+                InputSimulator isim = new InputSimulator();
+                isim.Keyboard.ModifiedKeyStroke(VirtualKeyCode.CONTROL, VirtualKeyCode.VK_C);
+
+                List<string> Filestodelete = new List<string>();
+                StringCollection files = Clipboard.GetFileDropList();
+                foreach (string item in files)
+                {
+                    if (File.Exists(item))
+                    {
+                        Filestodelete.Add(item);
+                    }
+                }
+
+                DialogResult result = MessageBox.Show("Do you really want to overwrite and delete these " + Filestodelete.Count + " Files?", "Delete and Overwrite", MessageBoxButtons.YesNo);
+                if (result == DialogResult.No)
+                {
+                    return;
+                }
+
+                foreach (string file in Filestodelete)
+                {
+                    long filesize = new FileInfo(file).Length;
+                    Stream fs = File.Open(file, FileMode.Open);
+                    for (int i2 = 0; i2 < 5; i2++)
+                    {
+                        fs.Position = 0;
+                        for (int i = 0; i < filesize; i++)
+                        {
+                            Byte[] b = new Byte[10];
+                            rnd.NextBytes(b);
+                            fs.WriteByte(b[rnd.Next(0, 10)]);
+                        }
+                    }
+                    fs.Close();
+                    File.Delete(file);
+                }
+            });
+
+            t.SetApartmentState(ApartmentState.STA);
+            t.Start();
+        }
+    }
+}
---
+++ OverwriteDeleteFiles/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("OverwriteDeleteFiles")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("OverwriteDeleteFiles")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c9ba2d73-5736-4fc0-9848-d18a61f8edca")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
