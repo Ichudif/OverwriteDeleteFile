3c57704493d2d91daed279e71a3f7d8ec806074d
Andreas Harapatt <andreas.harapatt@studierende.htl-donaustadt.at>
2019-07-20 13:28:29 +0200

    Added Folder deleting

--- OverwriteDeleteFiles/Program.cs
+++ OverwriteDeleteFiles/Program.cs
@@ -39,23 +39,41 @@ namespace OverwriteDeleteFiles
                 isim.Keyboard.ModifiedKeyStroke(VirtualKeyCode.CONTROL, VirtualKeyCode.VK_C);   //simulating ctrl + C
 
                 List<string> Filestodelete = new List<string>();
+                List<string> Folderstodelete = new List<string>();
                 StringCollection files = Clipboard.GetFileDropList();   //Getting the copied Files from the clipboard
                 foreach (string item in files)
                 {
-                    if (File.Exists(item))  //checking if these Files do exist
+                    if (File.Exists(item) || Directory.Exists(item))  //checking if these Files/Folders do exist
                     {
-                        Filestodelete.Add(item);
+                        GFRreturn gF = GetFilesRecursive(item);
+
+                        Filestodelete.AddRange(gF.FileList.Where(item2 => !Filestodelete.Contains(item2)));
+                        Folderstodelete.AddRange(gF.FolderList.Where(item2 => !Folderstodelete.Contains(item2)));
                     }
                 }
 
+                if (Filestodelete.Count == 0 && Folderstodelete.Count == 0)
+                {
+                    return;
+                }
+
                 //Ask if the user really wants to delete the selected Files, they will be completely destroyed
-                DialogResult result = MessageBox.Show("Do you really want to overwrite and delete these " + Filestodelete.Count + " Files?", "Delete and Overwrite", MessageBoxButtons.YesNo);
+                DialogResult result = MessageBox.Show("Do you really want to overwrite and delete " + Filestodelete.Count + " Files and " + Folderstodelete.Count + " Folders?", "Delete and Overwrite", MessageBoxButtons.YesNo);
                 if (result == DialogResult.No)
                 {
                     //user doesnt want, quitting ...
                     return;
                 }
-                
+
+                //deleting all aubfolders, as they will be deleted anyways, if the root folder is removed
+                for (int i = 0; i < Folderstodelete.Count; i++)
+                {
+                    Folderstodelete.Sort();                     //sorting - getting the shortest path first
+                    string selected = Folderstodelete[i];
+                    Folderstodelete.RemoveAll(item => item.Contains(selected + "\\"));     //removing all Paths, which contain the shortest path
+                    //Folderstodelete.Add(selected);          //re add the selected one, as he deletes himself -_-
+                }
+
                 //iterating over every File
                 foreach (string file in Filestodelete)
                 {
@@ -74,10 +92,53 @@ namespace OverwriteDeleteFiles
                     fs.Close();         //Close File
                     File.Delete(file);  //Delete File
                 }
+
+                Folderstodelete.ForEach(item => Directory.Delete(item, true));        //Deleting the Folders
+                MessageBox.Show("Finished");
+
+                Clipboard.Clear();      //Finally, clearing the Clipboard
             });
 
             t.SetApartmentState(ApartmentState.STA);    //Setting ApartmentState to STA => Clipboard
             t.Start();
+        }
+
+        /// <summary>
+        /// Return object of the GetFilesRecursive Method
+        /// </summary>
+        public struct GFRreturn
+        {
+            public List<string> FileList;
+            public List<string> FolderList;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="FolderPath">The Folder to search in</param>
+        /// <returns>An object, which contains a List of Files and a List of Folders inside the Folder, specified in FolderPath</returns>
+        public static GFRreturn GetFilesRecursive(string FolderPath)
+        {
+            List<string> FileList = new List<string>();
+            List<string> FolderList = new List<string>();
+            FolderList.Add(FolderPath);
+
+            foreach (string item in Directory.GetDirectories(FolderPath))
+            {
+                GFRreturn h = GetFilesRecursive(item);
+                FileList.AddRange(h.FileList);
+                FolderList.AddRange(h.FolderList);
+            }
+            foreach (string item in Directory.GetFiles(FolderPath))
+            {
+                FileList.Add(item);
+            }
+
+            GFRreturn returnme = new GFRreturn();
+            returnme.FileList = FileList;
+            returnme.FolderList = FolderList;
+
+            return returnme;
         }
     }
 }
